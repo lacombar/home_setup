6f9f71cbc3231bcb515284722598a2791690c5d6
Arnaud Lacombe <lacombar@gmail.com>
2016-05-13 10:21:43 -0700

    setup_repositories: simplify rpmfusion repo install

--- setup.sh
+++ setup.sh
@@ -64,6 +64,9 @@ setup_repositories()
 ${_content}
 EOF
 	done
+
+	sudo dnf install http://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	                 http://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 }
 
 setup_packages()
